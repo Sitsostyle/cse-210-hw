6050c40b38aecd97e033d0355a2df24bfc876cb0
Sitsostyle <71554005+Sitsostyle@users.noreply.github.com>
2025-05-15 10:00:11 -0700

    Aedded a comment

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -3,7 +3,15 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Sandbox Project.");
+    { 
+       // Ask the user for their name.
+
+       Console.Write("What is your first name? ");
+       string first=Console.ReadLine();
+
+       Console.Write("What is your last name? ");
+       string last=Console.ReadLine();
+
+       Console.WriteLine($"your name is {last},{first} {last}");
     }
 }
